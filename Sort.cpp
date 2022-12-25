e436b958a0705da291993397ec75203baaf2b70b
CarloFz <862638257@qq.com>
2022-12-25 20:48:19 +0800

    + Add create sort data.

--- Sort.cpp
+++ Sort.cpp
@@ -1,8 +1,28 @@
-
 #include <iostream>
+#include <fstream>
 using namespace std;
-int main()
+
+static void CreateData(int dataNum = 200)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    ofstream ofs;
+    ofs.open("SortData.txt", ios::out);
+    for(int i = 0; i < dataNum; i++)
+    {
+        ofs << i << endl;
+    }
+    ofs.close();
+    
+    ifstream ifs;
+    ifs.open("SortData.txt", ios::in);
+    
+    int d;
+    while(ifs >> d)
+    {
+        cout << d << endl;
+    }
+    ifs.close();
+}
+
+int main(int argc, const char * argv[]) {
+    CreateData();
 }
