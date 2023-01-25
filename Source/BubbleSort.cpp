c17bb49ca4af39c94533ad5323c65148088549c9
CarloFz <862638257@qq.com>
2023-01-25 23:21:40 +0800

    + add bubble sort

--- Source/BubbleSort.cpp
+++ Source/BubbleSort.cpp
@@ -5,6 +5,15 @@ namespace Encore
 {
     void SortAlgorithm::BubbleSort(vector<int>& array)
     {
-
+        int size = array.size();
+        for(int i = 0; i < size - 1; i++){ // loop n-1 times
+            for(int j = 0; j < size - i - 1; j++){ // compare elements(top i elements are ordered)
+                if(array[j] > array[j + 1]){
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
     }
 }
